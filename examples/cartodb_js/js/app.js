11f7606496587fd2ac89751f059660d3dc8bdb6f
Jorge Arévalo <jorgeas80@gmail.com>
2015-12-26 12:59:13 +0100

    Working in the CartoDB example. Now, showing standard infowindow and additional window with stats

--- examples/cartodb_js/js/app.js
+++ examples/cartodb_js/js/app.js
@@ -1,10 +1,42 @@
 // Generate HTML for infowindow
-function insert_data(content) {
-    var html_consent = "<h4>Nombre</h4><p>" + content.data.nombre + "</p>";
+function loadFeatureStats(info, cartodb_id) {
+    var sql = new cartodb.SQL({user: 'jorgeas80'});
 
-    return html_consent;
+    sql.execute("SELECT sum(st_length(b.the_geom_webmercator))/1000 as l, count(c.cartodb_id) as n FROM distritos_madrid a, ciclocarriles_madrid b, gbicimad c where st_contains(a.the_geom, b.the_geom) and st_contains(a.the_geom, c.the_geom) and a.cartodb_id = " + cartodb_id).done(function(data) {
+
+        if (data.rows) {
+            info.update(data.rows);
+        }
+
+    }).error(function(errors) {
+        // errors contains a list of errors
+        console.log("errors:" + errors);
+    });
 }
 
+
+function buildInfoWindowContent(data) {
+
+    if (data == 'waiting') {
+        return 'Cargando datos...';
+    }
+
+    var htmlDiv = "<div class='graph' style='right: 400px; top: 400px;'><div class='graph-inner'><ul class='graph-lst'>";
+
+    for(var i=0; i < data.length; i++) {
+        var h = parseInt(data[i].n) / 3;
+
+        htmlDiv += "<li style='height: " + h + "px'>";
+        htmlDiv += "<span class='graph-figure'>" + data[i].n + "</span>";
+        htmlDiv += "<span class='graph-provider'>" + data[i].l + "</span>";
+        htmlDiv += "</li>";
+
+    }
+
+    htmlDiv += "</ul><span class='bus'>Estaciones BiciMad</span> <span class='amount'>Km totales ciclocarril</span></div></div>";
+
+    return htmlDiv;
+}
 
 // Main function: create map and load cdb layers
 function main() {
@@ -12,17 +44,40 @@ function main() {
         var viz = cartodb.createVis('map', 'https://jorgeas80.cartodb.com/api/v2/viz/707b06ac-8ba8-11e5-bf74-0ea31932ec1d/viz.json', {
             shareable: false,
             search: false,
-            layer_selector: true,
         })
             .done(function (vis, layers) {
                 // To work with the native map
                 var map = vis.getNativeMap();
 
+                // Custom info window
+                var info = L.control();
+
+                info.onAdd = function (map) {
+                    this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+                    this.update();
+                    return this._div;
+                };
+
+                // method that we will use to update the control based on feature properties passed
+                info.update = function (data) {
+                    this._div.innerHTML = '<h4>Estadísticas BiciMad</h4>' +  (data ?
+                        buildInfoWindowContent(data)
+                        : 'Pincha en un distrito para ver sus estadísticas');
+                };
+
+                info.addTo(map);
+
+
                 // layer[1] is a layergroup containing the 3 sublayers (layer[0] is basemap)
                 pol_layer = layers[1].getSubLayer(0);
-                var infowindow = pol_layer.infowindow;
-                infowindow.set('template_type', 'underscore');
-                infowindow.set('template', $('#infowindow_template').html());
+                pol_layer.setInteraction(true);
 
+                // Update the info window with polygon stats
+                pol_layer.on('featureClick', function(e, pos, latlng, data) {
+                    loadFeatureStats(info, data.cartodb_id)
+                });
+
+            }).on('error', function() {
+                cartodb.log.log("some error occurred");
             });
 }
